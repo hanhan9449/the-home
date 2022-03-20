5a3c024f54aaf653dcbdf18bdb1d75f7cc6ae14e
韩涵 <hanhan.9449@bytedance.com>
2022-03-20 13:04:05 +0800

    feat: 添加室外天气模块

---
+++ fe/src/app/weather.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  private readonly key = 'bc948f6f64a84b768fff73684d5483c8';
+  private readonly geoInfo$ = this.getCurrentGeoInfo();
+
+  constructor(private httpClient: HttpClient) {}
+
+  getCurrentGeoInfo() {
+    let info$ = new BehaviorSubject<any>({
+      longitude: 104.07,
+      latitude: 30.66,
+    });
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        let info = {
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+        };
+        console.debug(info);
+        info$.next(info);
+      },
+      undefined,
+      { enableHighAccuracy: false }
+    );
+    return info$;
+  }
+
+  getWeatherInfo() {
+    const weatherUrl = 'https://devapi.qweather.com/v7/weather/now';
+
+    const params = {
+      key: this.key,
+      location: [
+        this.geoInfo$.getValue().longitude,
+        this.geoInfo$.getValue().latitude,
+      ].join(','),
+    };
+
+    const url = [
+      weatherUrl,
+      Object.entries(params)
+        .map(([a, b]) => [a, b].join('='))
+        .join('&'),
+    ].join('?');
+    const res$ = this.httpClient.get(url);
+    res$.subscribe((a) => console.debug(a));
+    return res$;
+  }
+}
