f6a6692a81559b111450a81a0dbe2cf638959b7b
韩涵 <hanhan.9449@bytedance.com>
2022-03-20 13:40:06 +0800

    feat: 支持跳转查看更多信息

--- fe/src/app/home/home.module.ts
+++ fe/src/app/home/home.module.ts
@@ -1,21 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import {DeviceInfoService} from "../device-info.service";
-import {ServerService} from "../server.service";
-import {HttpClientModule} from "@angular/common/http";
-
-
+import { DeviceInfoService } from '../device-info.service';
+import { ServerService } from '../server.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ],
-  providers: [
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HttpClientModule, MatButtonModule],
+  providers: [],
 })
-export class HomeModule { }
+export class HomeModule {}
